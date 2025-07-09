96438e949f32f0eeaa7c18a2a3492d09b4fadd15
Utku Sarioglu <utkusarioglu@hotmail.com>
2025-07-09 16:43:49 +0000

    Feat: Tooling updates 1

--- src/home/scripts/bootstrap/c/set-clang-ln.sh
+++ src/home/scripts/bootstrap/c/set-clang-ln.sh
@@ -3,5 +3,7 @@
 set -euxo pipefail
 bash --version
 
-ln -s /usr/bin/clang-14 /usr/local/bin/clang
-ln -s /usr/bin/clang++-14 /usr/local/bin/clang++
+# ln -s /usr/bin/clang-14 /usr/local/bin/clang
+# ln -s /usr/bin/clang++-14 /usr/local/bin/clang++
+ln -s /usr/bin/clang-20 /usr/local/bin/clang
+ln -s /usr/bin/clang++-20 /usr/local/bin/clang++
